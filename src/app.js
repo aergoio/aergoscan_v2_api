afdf67f515784371957fa1a64b78343b8f384ca8
william <cwlim0305@gmail.com>
2023-04-05 17:46:04 +0900

    add: ip blacklist - testnet

--- src/app.js
+++ src/app.js
@@ -21,10 +21,79 @@ import * as path from "path";
 
 const specs = YAML.load(path.join(__dirname, '../swagger/aergoscan-api.yaml'))
 
+// blacklsit ip
+const blacklist = [
+'180.230.34.43',
+'121.179.231.119',
+'203.248.117.37',
+'77.191.172.239',
+'165.225.228.86',
+'99.182.41.248',
+'222.112.179.205',
+'121.152.216.248',
+'211.202.159.170',
+'2a02:a460:5167:1:ea19:fe10:54a4:aa43',
+'64.64.123.40',
+'106.249.246.242',
+'2001:e60:3164:fca0:5c58:f990:29db:5f6f',
+'122.39.239.101',
+'211.180.247.136',
+'2601:243:2381:ba40:9897:c55b:dc78:5ddc',
+'177.97.71.200',
+'118.38.119.193',
+'222.235.58.103',
+'211.114.70.66',
+'85.249.169.160',
+'218.39.98.234',
+'121.157.43.31',
+'59.152.147.103',
+'211.243.232.72',
+'116.121.168.102',
+'31.187.80.43',
+'182.218.228.112',
+'125.143.174.77',
+'180.64.248.30',
+'221.143.199.92',
+'211.214.21.200',
+'175.157.246.179',
+'125.242.37.26',
+'59.171.220.62',
+'210.117.213.228',
+'211.192.221.21',
+'85.8.117.99',
+'59.16.52.236',
+'18.141.197.1',
+'154.6.151.173',
+'82.41.32.174',
+'123.140.248.18',
+'211.203.100.121',
+'203.220.226.1',
+'175.197.211.178',
+'49.142.68.249',
+'125.139.33.152',
+'116.45.149.164',
+'125.183.156.158',
+'118.42.164.202',
+'45.149.175.69',
+'211.52.165.168',
+'211.226.203.222',
+'211.40.136.213',
+'211.108.111.47',
+'222.109.19.241',
+'118.129.66.2',
+'161.122.52.167',
+'211.255.203.42',
+'2001:448a:6040:b2cd:b0cd:c13:d348:1e40',
+'220.87.212.48',
+'121.167.146.95',
+'119.195.243.218'
+];
+
 const app = express();
 
 // app.use(cors({credentials: true, origin: 'http://localhost:8081'}));
 app.use(cors({credentials: true, origin: true}));
+
 
 // Nested router for chainId
 const chainRouter = express.Router();
@@ -34,10 +103,7 @@ const cache = {
     swapDataUpdated: null,
 };
 
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs))
-
-
 
 app.use('/', chainRouter);
 app.use((err, req, res, next) => {
@@ -48,6 +114,18 @@ app.use((err, req, res, next) => {
 });
 
 chainRouter.route('/').get((req, res) => {
+
+    // ip filter
+    const requestIP = req.connection.remoteAddress;
+    console.log(requestIP);
+    console.log(blacklist.indexOf(requestIP))
+    if(blacklist.indexOf(requestIP) >= 0) {
+        console.log('aaccess attempt : ' + requestIP)
+        return res.json({
+            msg: 'Access Denied'
+        });
+    }
+
     return res.json({
         msg: 'Welcome to the Aergoscan API. Please select a chain id.',
         // chains: cfg.AVAILABLE_NETWORKS.map(chainId => `${cfg.HOST}/${chainId}/${cfg.VERSION}`)

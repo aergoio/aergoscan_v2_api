9425d7268f7b7ae5d735adda399a3138c999c04c
william <cwlim0305@gmail.com>
2023-04-13 14:21:22 +0900

    update: deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,20 +1,32 @@
+const ENV_SELECTED_NETWORK = process.env.SELECTED_NETWORK
+const ENV_ES_URL = process.env.ES_URL;
+const ENV_HTTP_PORT = process.env.HTTP_PORT;
+const ENV_HOST_API = process.env.HOST_API;
 
-console.log(">>> process.env.SELECTED_NETWORK = "+process.env.SELECTED_NETWORK);
-console.log(">>> process.env.ES_URL           = "+process.env.ES_URL);
-console.log(">>> process.env.HTTP_PORT        = "+process.env.HTTP_PORT);
+console.log(">>> process.env.SELECTED_NETWORK = "+ENV_SELECTED_NETWORK);
+console.log(">>> process.env.ES_URL           = "+ENV_ES_URL);
+console.log(">>> process.env.ENV_HTTP_PORT    = "+ENV_HTTP_PORT);
+console.log(">>> process.env.HTTP_HOST_API    = "+ENV_HOST_API);
 
-console.log("............"+process.env.SELECTED_NETWORK)
+module.exports = {
+    // api version
+    VERSION: "v2",
 
-if (process.env.SELECTED_NETWORK == 'local') {
-    console.log("aergoscan local Mode");
-    module.exports = require('./config-local');
-} else if (process.env.SELECTED_NETWORK == 'testnet') {
-    console.log("aergoscan testnet Mode");
-    module.exports = require('./config-testnet');
-} else if (process.env.SELECTED_NETWORK == 'mainnet') {
-    console.log("aergoscan mainnet Mode");
-    module.exports = require('./config-mainnet');
-} else if (process.env.SELECTED_NETWORK == 'alpha') {
-    console.log("aergoscan alpha Mode");
-    module.exports = require('./config-alpha');
-}
+    HTTP_PORT: ENV_HTTP_PORT,
+    DB_HOST: process.env.ES_HOST || ENV_ES_URL,
+    AVAILABLE_NETWORKS: [ENV_HOST_API],
+    HOST: ENV_HOST_API,
+
+    //-- node grpc
+    NODE_GRPC_MAIN: 'mainnet-api.aergo.io:7845',
+    NODE_GRPC_TEST: 'testnet-api.aergo.io:7845',
+    NODE_GRPC_ALPHA: 'alpha-api.aergo.io:7845',
+
+    //-- Scheduler Config
+    SCHEDULER_NETWORK: ENV_SELECTED_NETWORK,
+    SCHEDULER_BASEURL:  'http://127.0.0.1:'+ENV_HTTP_PORT+'/',
+
+    // Registered Token Info (default info (image/url)
+    UNREGISTERED_TOKEN_URL: '',
+    UNREGISTERED_TOKEN_IMAGE: ''
+};

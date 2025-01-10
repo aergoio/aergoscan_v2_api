9c94731b1ef3d5b11eb8d980c91d0aff7ff80dd4
findmytrueself <load1999@gmail.com>
2025-01-10 18:56:16 +0900

    Feat: Modify the implementation to retrieve gRPC data through the backend

--- src/api/v2/mainApi.js
+++ src/api/v2/mainApi.js
@@ -1,15 +1,19 @@
-import express from "express";
+import express from 'express'
 
 import {
-    txHistory,
-    CachedTxHistory,
-    mainBlockInfo,
-    CachedMainBlockInfo,
-    RecentTransactions,
-    CachedRecentTransactions,
-} from "../../services/v2/mainService";
+  txHistory,
+  CachedTxHistory,
+  mainBlockInfo,
+  CachedMainBlockInfo,
+  RecentTransactions,
+  CachedRecentTransactions,
+  getPeerInfo,
+  getChainInfo,
+  getConsensusInfo,
+  getBestBlock,
+} from '../../services/v2/mainService'
 
-const apiV2 = express.Router({ mergeParams: true });
+const apiV2 = express.Router({ mergeParams: true })
 
 /**
  * @swagger
@@ -23,11 +27,18 @@ const apiV2 = express.Router({ mergeParams: true });
  *   - https
  *   - http
  */
-apiV2.route("/txHistory").get(txHistory);
-apiV2.route("/CachedTxHistory").get(CachedTxHistory);
-apiV2.route("/mainBlockInfo").get(mainBlockInfo);
-apiV2.route("/CachedMainBlockInfo").get(CachedMainBlockInfo);
-apiV2.route("/RecentTransactions").get(RecentTransactions);
-apiV2.route("/CachedRecentTransactions").get(CachedRecentTransactions);
 
-export default apiV2;
+apiV2.route('/txHistory').get(txHistory)
+apiV2.route('/CachedTxHistory').get(CachedTxHistory)
+apiV2.route('/mainBlockInfo').get(mainBlockInfo)
+apiV2.route('/CachedMainBlockInfo').get(CachedMainBlockInfo)
+apiV2.route('/RecentTransactions').get(RecentTransactions)
+apiV2.route('/CachedRecentTransactions').get(CachedRecentTransactions)
+
+// get blockchain datas by hera.js
+apiV2.route('/getPeerInfo').get(getPeerInfo)
+apiV2.route('/getChainInfo').get(getChainInfo)
+apiV2.route('/getConsensusInfo').get(getConsensusInfo)
+apiV2.route('/getBestBlock').get(getBestBlock)
+
+export default apiV2
--- src/services/v2/mainService.js
+++ src/services/v2/mainService.js
@@ -1,35 +1,32 @@
-import { schedulerDataCache } from "../../caches/caches";
-import { heraGrpcProvider } from "../herajs";
+import { schedulerDataCache } from '../../caches/caches'
+import { heraGrpcProvider } from '../herajs'
 
 /**
  * txHistory
  */
 const txHistory = async (req, res, next) => {
-    console.log("txHistory!!");
-    try {
-        // if(!schedulerDataCache.has('txHistory')){
-        //-- CachedTxHistory 에 있는 함수랑 같음.
-        const [txPerDay] = await Promise.all([
-            req.apiClient.aggregateBlocks(
-                { gte: "now-13d/d", lte: "now" },
-                "1d"
-            ),
-        ]);
+  console.log('txHistory!!')
+  try {
+    // if(!schedulerDataCache.has('txHistory')){
+    //-- CachedTxHistory 에 있는 함수랑 같음.
+    const [txPerDay] = await Promise.all([
+      req.apiClient.aggregateBlocks({ gte: 'now-13d/d', lte: 'now' }, '1d'),
+    ])
 
-        schedulerDataCache.set("txHistory_" + process.env.SELECTED_NETWORK, {
-            txPerDay,
-        });
-        // }
+    schedulerDataCache.set('txHistory_' + process.env.SELECTED_NETWORK, {
+      txPerDay,
+    })
+    // }
 
-        // return res.json(schedulerDataCache.get('txHistory_'+process.env.SELECTED_NETWORK));
-        return res.json({
-            txPerDay,
-        });
-    } catch (e) {
-        console.log("........" + e);
-        return res.json({ error: e });
-    }
-};
+    // return res.json(schedulerDataCache.get('txHistory_'+process.env.SELECTED_NETWORK));
+    return res.json({
+      txPerDay,
+    })
+  } catch (e) {
+    console.log('........' + e)
+    return res.json({ error: e })
+  }
+}
 
 /**
  * CachedSchedule - txHistory
@@ -38,43 +35,23 @@ const txHistory = async (req, res, next) => {
  * key list - ['mainBlockInfo_alpha', 'recentTransactions_alpha', 'txHistory_alpha']
  */
 const CachedTxHistory = async (req, res, next) => {
-    // console.log('CachedTxHistory!!');
-    // console.log('qeury = '+req.query);
-    try {
-        const [txPerDay] = await Promise.all([
-            req.apiClient.aggregateBlocks(
-                { gte: "now-30d/d", lte: "now" },
-                "1d"
-            ),
-        ]);
+  // console.log('CachedTxHistory!!');
+  // console.log('qeury = '+req.query);
+  try {
+    const [txPerDay] = await Promise.all([
+      req.apiClient.aggregateBlocks({ gte: 'now-30d/d', lte: 'now' }, '1d'),
+    ])
 
-        schedulerDataCache.set(req.query.key, {
-            txPerDay,
-        });
+    schedulerDataCache.set(req.query.key, {
+      txPerDay,
+    })
 
-        return res.json({
-            txPerDay,
-        });
-    } catch (e) {
-        return res.json({ error: e });
-    }
-};
-
-const peerInfo = async (req, res, next) => {
-    console.log("peerInfo url : " + req.url);
-
-    let aergoClientType = heraGrpcProvider(process.env.SELECTED_NETWORK);
-    try {
-        try {
-            let peers = await aergoClientType.getPeers();
-            // console.log(JSON.stringify(peers));
-            return res.json(peers);
-        } catch (e) {
-            console.log("connectState e = " + e);
-        }
-    } catch (e) {
-        return res.json({ error: e });
-    }
+    return res.json({
+      txPerDay,
+    })
+  } catch (e) {
+    return res.json({ error: e })
+  }
 }
 
 /**
@@ -82,17 +59,15 @@ const peerInfo = async (req, res, next) => {
  *  - BP number, Latest Block - hera 에서 가져옴
  */
 const mainBlockInfo = async (req, res, next) => {
-    // console.log('mainBlockInfo!!');
-    try {
-        return res.json(
-            schedulerDataCache.get(
-                "mainBlockInfo_" + process.env.SELECTED_NETWORK
-            )
-        );
-    } catch (e) {
-        return res.json({ error: e });
-    }
-};
+  // console.log('mainBlockInfo!!');
+  try {
+    return res.json(
+      schedulerDataCache.get('mainBlockInfo_' + process.env.SELECTED_NETWORK)
+    )
+  } catch (e) {
+    return res.json({ error: e })
+  }
+}
 
 /**
  * CachedSchedule - CacehMainBlockInfo
@@ -101,52 +76,52 @@ const mainBlockInfo = async (req, res, next) => {
  * key list - ['mainBlockInfo_alpha', 'recentTransactions_alpha', 'txHistory_alpha']
  */
 const CachedMainBlockInfo = async (req, res, next) => {
-    // apiRouterV2.route('/CachedMainBlockInfo').get(routeCache.cacheSeconds(5), async (req, res) => {
-    // apiRouterV2.route('/CachedMainBlockInfo').get(async (req, res) => {
-    // console.log('CachedMainBlockInfo!!');
-    // console.log('CachedMainBlockInfo!!' + JSON.stringify(req.query.key));
-    try {
-        const [
-            // txPerMonth,
-            txTotal,
-            maxTps,
-            maxTpm,
-            blockCount,
-        ] = await Promise.all([
-            // req.apiClient.aggregateBlocks({ gte: "now-10y/y", lt: "now" }, "1M"),
-            req.apiClient.getTxCount(),
+  // apiRouterV2.route('/CachedMainBlockInfo').get(routeCache.cacheSeconds(5), async (req, res) => {
+  // apiRouterV2.route('/CachedMainBlockInfo').get(async (req, res) => {
+  // console.log('CachedMainBlockInfo!!');
+  // console.log('CachedMainBlockInfo!!' + JSON.stringify(req.query.key));
+  try {
+    const [
+      // txPerMonth,
+      txTotal,
+      maxTps,
+      maxTpm,
+      blockCount,
+    ] = await Promise.all([
+      // req.apiClient.aggregateBlocks({ gte: "now-10y/y", lt: "now" }, "1M"),
+      req.apiClient.getTxCount(),
 
-            req.apiClient.searchBlock(
-                {
-                    body: {
-                        size: 1,
-                        sort: {
-                            txs: { order: "desc" },
-                        },
-                    },
-                },
-                true
-            ),
-            req.apiClient.getBestBlock(),
-            req.apiClient.getBlockCount(),
-        ]);
-        // const txTotal = txPerMonth.map(day => day.sum_txs.value).reduce((a, b) => a + b, 0);
+      req.apiClient.searchBlock(
+        {
+          body: {
+            size: 1,
+            sort: {
+              txs: { order: 'desc' },
+            },
+          },
+        },
+        true
+      ),
+      req.apiClient.getBestBlock(),
+      req.apiClient.getBlockCount(),
+    ])
+    // const txTotal = txPerMonth.map(day => day.sum_txs.value).reduce((a, b) => a + b, 0);
 
-        schedulerDataCache.set(req.query.key, {
-            blockCount,
-            txTotal,
-            maxTps,
-            maxTpm,
-        });
+    schedulerDataCache.set(req.query.key, {
+      blockCount,
+      txTotal,
+      maxTps,
+      maxTpm,
+    })
 
-        return res.json({
-            blockCount,
-            txTotal,
-            maxTps,
-            maxTpm,
-        });
+    return res.json({
+      blockCount,
+      txTotal,
+      maxTps,
+      maxTpm,
+    })
 
-        /*
+    /*
         const [
             // txPerMonth,
             txTotal,
@@ -189,27 +164,27 @@ const CachedMainBlockInfo = async (req, res, next) => {
 
         })
         */
-    } catch (e) {
-        return res.json({ error: e });
-    }
-};
+  } catch (e) {
+    return res.json({ error: e })
+  }
+}
 
 /**
  * Recent transactions
  */
 const RecentTransactions = async (req, res, next) => {
-    // console.log('recentTransactions!!');
-    try {
-        // return res.json(schedulerDataCache.get('recentTransactions_'+req.params.chainId));
-        return res.json(
-            schedulerDataCache.get(
-                "recentTransactions_" + process.env.SELECTED_NETWORK
-            )
-        );
-    } catch (e) {
-        return res.json({ error: e });
-    }
-};
+  // console.log('recentTransactions!!');
+  try {
+    // return res.json(schedulerDataCache.get('recentTransactions_'+req.params.chainId));
+    return res.json(
+      schedulerDataCache.get(
+        'recentTransactions_' + process.env.SELECTED_NETWORK
+      )
+    )
+  } catch (e) {
+    return res.json({ error: e })
+  }
+}
 
 /**
  * CachedSchedule - recentTransactions
@@ -218,34 +193,112 @@ const RecentTransactions = async (req, res, next) => {
  * key list - ['mainBlockInfo_alpha', 'recentTransactions_alpha', 'txHistory_alpha']
  */
 const CachedRecentTransactions = async (req, res, next) => {
-    // apiRouterV2.route('/CachedRecentTransactions').get(routeCache.cacheSeconds(5), async (req, res) => {
-    // apiRouterV2.route('/CachedRecentTransactions').get(async (req, res) => {
-    //     console.log('CachedRecentTransactions!!');
+  // apiRouterV2.route('/CachedRecentTransactions').get(routeCache.cacheSeconds(5), async (req, res) => {
+  // apiRouterV2.route('/CachedRecentTransactions').get(async (req, res) => {
+  //     console.log('CachedRecentTransactions!!');
+  try {
+    const txList = await req.apiClient.searchTransactions({
+      match_all: {},
+    })
+
+    schedulerDataCache.set(req.query.key, {
+      txList,
+    })
+
+    // console.log(CachedSchedule.get('recentTransactions_'+req.params.chainId));
+
+    return res.json({
+      txList,
+    })
+  } catch (e) {
+    return res.json({ error: e })
+  }
+}
+
+/**
+ * @fileoverview Provides gRPC data from Hera.js with direct database bypassing.
+ * @description This module handles blockchain-related data
+ * by directly interacting with Hera.js gRPC client.
+ */
+
+/**
+ * Fetch peer information from the blockchain network.
+ * Bypasses the database and directly retrieves data from the gRPC client.
+ */
+
+const getPeerInfo = async (req, res, next) => {
+  console.log('peerInfo url : ' + req.url)
+
+  let aergoClientType = heraGrpcProvider(process.env.SELECTED_NETWORK)
+  try {
     try {
-        const txList = await req.apiClient.searchTransactions({
-            match_all: {},
-        });
-
-        schedulerDataCache.set(req.query.key, {
-            txList,
-        });
-
-        // console.log(CachedSchedule.get('recentTransactions_'+req.params.chainId));
-
-        return res.json({
-            txList,
-        });
+      let peers = await aergoClientType.getPeers()
+      // console.log(JSON.stringify(peers));
+      return res.json(peers)
     } catch (e) {
-        return res.json({ error: e });
+      console.log('connectState e = ' + e)
     }
-};
+  } catch (e) {
+    return res.json({ error: e })
+  }
+}
 
+const getChainInfo = async (req, res, next) => {
+  console.log('chainInfo url : ' + req.url)
+
+  let aergoClientType = heraGrpcProvider(process.env.SELECTED_NETWORK)
+  try {
+    try {
+      let chainInfo = await aergoClientType.getChainInfo()
+      return res.json(chainInfo)
+    } catch (e) {
+      console.log('connectState e = ' + e)
+    }
+  } catch (e) {
+    return res.json({ error: e })
+  }
+}
+
+const getConsensusInfo = async (req, res, next) => {
+  console.log('consensusInfo url : ' + req.url)
+
+  let aergoClientType = heraGrpcProvider(process.env.SELECTED_NETWORK)
+  try {
+    try {
+      let consensusInfo = await aergoClientType.getConsensusInfo()
+      return res.json(consensusInfo)
+    } catch (e) {
+      console.log('connectState e = ' + e)
+    }
+  } catch (e) {
+    return res.json({ error: e })
+  }
+}
+
+const getBestBlock = async (req, res, next) => {
+  console.log('bestBlock url : ' + req.url)
+
+  let aergoClientType = heraGrpcProvider(process.env.SELECTED_NETWORK)
+  try {
+    try {
+      let bestBlock = await aergoClientType.blockchain()
+      return res.json(bestBlock)
+    } catch (e) {
+      console.log('connectState e = ' + e)
+    }
+  } catch (e) {
+    return res.json({ error: e })
+  }
+}
 export {
-    peerInfo,
-    txHistory,
-    CachedTxHistory,
-    mainBlockInfo,
-    CachedMainBlockInfo,
-    RecentTransactions,
-    CachedRecentTransactions,
-};
+  txHistory,
+  CachedTxHistory,
+  mainBlockInfo,
+  CachedMainBlockInfo,
+  RecentTransactions,
+  CachedRecentTransactions,
+  getPeerInfo,
+  getChainInfo,
+  getConsensusInfo,
+  getBestBlock,
+}

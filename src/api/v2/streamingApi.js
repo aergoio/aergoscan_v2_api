41b42c46b6531a62a409343605f403684f5800e4
findmytrueself <load1999@gmail.com>
2025-01-10 18:55:21 +0900

    remove

--- src/api/v2/streamingApi.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { peerInfo } from '../../services/v2/streamingService'
-
-const apiV2 = express.Router({ mergeParams: true })
-
-apiV2.route('/peerInfo').get(peerInfo)
-
-export default apiV2
